bf7c05c2384ca76d4f98d4726df6ea3ebfa689e0
Lesego2022 <lesegomasakana@gmail.com>
2023-03-10 08:01:23 -0800

    update

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,10 +1,14 @@
 #include "main.h"
- /**
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
  *
- * Return: void	
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
@@ -23,7 +27,7 @@ char *_strcat(char *dest, char *src)
 		dest[i] = src[j];
 		i++;
 		j++;
-	}		
+	}
 
 
 	dest[i] = '\0';
