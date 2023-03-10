1b2647b64748b91ec60ce7a973d70abb4c28c8f9
Lesego2022 <lesegomasakana@gmail.com>
2023-03-10 12:02:44 -0800

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -18,15 +18,3 @@ void _puts(char *str)
 		i++;
 	}
 }
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
-*/
-
-int main(void)
-{
-	_puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
