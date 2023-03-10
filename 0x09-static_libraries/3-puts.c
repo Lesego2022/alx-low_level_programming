1f29c06c248d560733f11aaa2c945f714232502a
Lesego2022 <lesegomasakana@gmail.com>
2023-03-10 08:47:52 -0800

    update

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,17 +1,32 @@
-#include "main.h"
 #include <stdio.h>
+
+/**
+ * _puts - Prints a string to stdout followed by a new line.
+ *
+ * @str: pointer to the string to be printed.
+ *
+ * Return: None.
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+}
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */
-void _puts(char *str)
+*/
+
+int main(void)
 {
-	int i = 0;
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+	_puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
